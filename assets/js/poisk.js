920705194fcf546677fa3e5eb7189316f3dff924
sineokyone <sineoky.an@ya.ru>
2022-04-29 23:59:33 +0400

    upd

---
+++ assets/js/poisk.js
@@ -0,0 +1,30 @@
+alert( "Салат аливейкум!!" );
+
+const p= prompt('Что над найти?', '');
+const pr = ('../index.html');
+const ur = ('https://yandex.ru/search/?text=' + p);
+	
+	function poisk(){
+	document.location.href = ur;
+	}
+
+
+	if(p==null){
+	alert("Отменил? -Больше не обращайся..")
+	document.location.href = pr;
+	}else{sp()
+     	}
+
+
+
+	function sp() {
+		if(p==""){ //  -- || - ИЛИ 
+		alert( "Неее братух, мне тут пустышки твои не нужны\nиди-ка погуляй" );
+		document.location.href = pr;
+	}
+	else{
+		poisk()
+		// 	document.location.href = ur;
+// 	alert("Для поиска по запросу: "+ p+"\nНажми ОК" );
+	}
+	}
---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+console.log("Закрой эту консоль быстро!!!!"+"\n"+"И вапще не лезь сюда больше понел?!?!?!")
+
+document.querySelector("button").onclick = app;
+
+
+
+
+function ape(){
+	let inq=document.querySelector("input").value
+	document.location.href = ('https://yandex.ru/search/?text='+inq)
+	document.getElementById('val1').value = '';
+}
+
+	
+	
+
+
+function app(){
+	const letters = /^[a-zA-Z0-9]+$/;
+	
+	let inp = document.querySelector("input").value
+	
+	let ur = ('https://yandex.ru/search/?text='+ inp)
+	
+	if((inp =="")||(inp==" ")){
+		alert( "Неее братух, мне тут пустышки твои не нужны\nиди-ка погуляй" );
+	}else{ 
+	
+		ape()
+	}	
+}
