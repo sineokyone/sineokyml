c930ea97ff65a6a9918268fec25552e44dafff1a
Andrew <77820712+sineokyone@users.noreply.github.com>
2022-05-01 03:56:48 +0400

    E

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,8 +1,6 @@
 window.onload = function () {
 	localStorage.getItem("background")
     document.body.style.background = localStorage.getItem("background");
-}
-window.onload = function () {
 	localStorage.getItem("searchInfo")
     searchSystem = localStorage.getItem("searchInfo");
 }
